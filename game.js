e7c83cf3b47744ee98d77bdea63ad92ed3f993d1
Jacob <joyear62@harrisonburg.k12.va.us>
2019-09-18 08:52:09 -0400

    idle-game

--- game.js
+++ game.js
@@ -6,8 +6,6 @@ createButton('Button', btnPress);
 createButton('Button', btnPress);
 changeTitle('Clicker Game');
 
-setInterval(btnPress, 1000);
-
 function btnPress() {
   score++;
   postToPage(score);
