e2520f2fda0cee0439dd708020c1fb8186fcd603
Jacob <joyear62@harrisonburg.k12.va.us>
2019-09-19 09:55:37 -0400

    Ice Cream Enterprise

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 var score = 0; // Create a variable to store the score in
 
-var title = new Title('Ice Cream Enterpise'); // Create a title element
+var title = new Title('Ice Cream Enterprise'); // Create a title element
 var button = new Button('Press', btnPress);   // Create a button element
 var hidden = new Button('Press', btnPress);
 hidden.hide();
@@ -11,8 +11,7 @@ setInterval(btnPress, 1000);   // Set up a loop
 function btnPress() {
   score++;         // Increase the score
   scr.edit(score); // Update the page with the new score
-  if(score > 10) {
-    hidden.show();
-
+  if(score > 99) {
+    hidden.show(); // Shows the button
   }
 }
