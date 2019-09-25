99136e21ff9ba7e2589f0e99833aac21b5aeb250
Jacob <joyear62@harrisonburg.k12.va.us>
2019-09-25 09:54:13 -0400

    dsa

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 var score = 0; // Create a variable to store the score in
 
 var title = new Title('Ice Cream Enterprise'); // Create a title element
-var button = new Button('Press', btnPress);   // Create a button element
-var hidden = new Button('Press', btnPress);
+var button = new Button('Make and Sell Ice Cream' , btnPress);   // Create a button element
+var hidden = new Button('Get Higher Quality Ice Cream', btnPress1);
 hidden.hide();
 var scr = new Text(score);                    // Create a text element
 //changes
@@ -13,5 +13,12 @@ function btnPress() {
   scr.edit(score); // Update the page with the new score
   if(score > 99) {
     hidden.show(); // Shows the button
+  }
+}
+
+function btnPress1() { 
+  if(btnPress1 = True) {
+  score - 100;
+  scr.edit(score);
   }
 }
