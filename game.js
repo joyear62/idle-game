f2e97f744635f8ff4e6f73e5bd8ad98a44a5c0b3
Jacob <joyear62@harrisonburg.k12.va.us>
2019-09-26 09:54:07 -0400

    idle-game

--- game.js
+++ game.js
@@ -1,24 +1,24 @@
-var score = 0; // Create a variable to store the score in
+var section = new Section();
+var text = new Text();
+section.add(text);
+section.addBorder();
 
-var title = new Title('Ice Cream Enterprise'); // Create a title element
+var score = 0; // Creates a variable to store the score in
+var title = new Title('Ice Cream Enterprise'); // Creates a title element
 var button = new Button('Make and Sell Ice Cream' , btnPress);   // Create a button element
-var hidden = new Button('Get Higher Quality Ice Cream', btnPress1);
+var hidden = new Button('Get Higher Quality Ice Cream', btnPress);
 hidden.hide();
-var scr = new Text(score);                    // Create a text element
+var scr = new Text(score);                    // Creates a text element
 //changes
-setInterval(btnPress, 1000);   // Set up a loop
+setInterval(btnPress, 1000);   // Sets up a loop
 
 function btnPress() {
-  score++;         // Increase the score
-  scr.edit(score); // Update the page with the new score
+  score++;         // Increases the score
+  scr.edit(score); // Updates the page with the new score
   if(score > 99) {
     hidden.show(); // Shows the button
-  }
-}
-
-function btnPress1() { 
-  if(btnPress1 = True) {
-  score - 100;
-  scr.edit(score);
+  } else {
+    if(score < 100) 
+    hidden.hide()  // Hides the button
   }
 }
